4a6be17c06c09e78c48cbc91b1a4d35d057153a4
Walid Maarad <walsam@hotmail.com>
2020-11-27 17:13:19 +0100

    feature: added our data structure where we will save out data, and read the data from the input.

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
-    //some code
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //create new id
+            if (data.allItems[type].length>0) {
+                ID=data.allItems[type][data.allItems[type].length-1].id+1;
+            } else {ID=0;}
+
+            //create new item (inc or exp)
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val) ;
+            } else if (type==='inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            // push it into our data structure
+            data.allItems[type].push(newItem);
+            //return it
+            return newItem;
+
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    }
+
 })();
 
 
@@ -48,11 +99,13 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function () {
+        var input, newItem;
 
         // 1. get the input data
-        var input = UICtrl.getinput();
+        input = UICtrl.getinput();
 
         // 2. add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. add the item to the UI
 
