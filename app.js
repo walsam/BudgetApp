25f5c5fc483828b388ea2afa4a5b29d7d44a47fe
Walid Maarad <walsam@hotmail.com>
2020-11-30 02:40:15 +0100

    feature: showing the year and the month

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ var UIController = (function (){
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
         expensesPercLabel: '.item__percentage',
-
+        dateLabel: '.budget__title--month'
     }
 
     var formatNumber = function(num, type) {
@@ -283,6 +283,18 @@ var UIController = (function (){
 
         },
 
+        displayMonth: function() {
+            var now, months, month, year;
+
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -395,6 +407,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('the application has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
